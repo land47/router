e1c92cd5f2dd458cea8a122bfa90217be28a0c9c
Anton <jjoska@yandex.ru>
2020-12-13 01:09:40 +0300

    feat(utils/isEqualArrays): initialize helper for compare two arrays

---
+++ src/utils/isEqualArrays.ts
@@ -0,0 +1,11 @@
+export function isEqualsArrays<T>(a: T[], b: T[]) {
+  if (a.length !== b.length) {
+    return false
+  }
+
+  if (a === b) {
+    return true
+  }
+
+  return a.every(e => b.find(el => el === e))
+}
