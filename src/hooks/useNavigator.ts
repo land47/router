9523662887b3622e99de03302208e03a05e4ab76
Anton <jjoska@yandex.ru>
2020-12-13 16:59:14 +0300

    chore(hooks/useNavigator): throw an error if navigator is null

--- src/hooks/useNavigator.ts
+++ src/hooks/useNavigator.ts
@@ -1,11 +1,11 @@
 import { useContext } from 'react'
-import { NavigatorContext } from 'contexts'
+import { NavigatorContext } from '../contexts'
 
 export function useNavigator() {
   let navigator = useContext(NavigatorContext)
 
   if (navigator === null) {
-    throw new Error('TODO')
+    throw new Error('The application must be wrapped in <Router />')
   }
 
   return navigator
