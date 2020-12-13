1c2f82ee81f841bfb8ef75a660d0016828c26bd1
Anton <jjoska@yandex.ru>
2020-12-13 20:28:17 +0300

    feat: re-export `Router` from components

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Navigator } from './base'
 export { useNavigator, useStructure, useRouter } from './hooks'
 export { NavigatorContext } from './contexts'
+export { Router } from './components'
