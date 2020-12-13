de3e3358428033eae2e64ae7e41d43841939c3dc
Anton <jjoska@yandex.ru>
2020-12-13 21:30:25 +0300

    feat: re-export `useParams()` from hooks

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { Navigator } from './base'
-export { useNavigator, useStructure, useRouter } from './hooks'
+export { useNavigator, useStructure, useRouter, useParams } from './hooks'
 export { NavigatorContext } from './contexts'
 export { Router } from './components'
