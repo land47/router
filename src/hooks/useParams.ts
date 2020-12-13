4cc9ca72c940618d11d85e84ea90457b31836736
Anton <jjoska@yandex.ru>
2020-12-13 21:33:37 +0300

    docs(hooks/useParams): initialized docs

--- src/hooks/useParams.ts
+++ src/hooks/useParams.ts
@@ -1,5 +1,8 @@
 import { useNavigator } from '../hooks'
 
+/**
+ * Возвращает состояние (параметры) текущей записи в истории.
+ * */
 export function useParams() {
   let navigator = useNavigator()
   return navigator.history.state
