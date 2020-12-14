1e4aa692f456eef8bbd8c1d2cb493c024cc5c56d
Anton <jjoska@yandex.ru>
2020-12-14 19:23:04 +0300

    feat: re-export `useSnackbar()` (#1)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 export { Navigator } from './base'
-export { useNavigator, useStructure, useRouter, useParams } from './hooks'
+export {
+  useNavigator,
+  useStructure,
+  useRouter,
+  useParams,
+  useSnackbar,
+} from './hooks'
 export { NavigatorContext } from './contexts'
 export { Router } from './components'
