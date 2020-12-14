f689f54790176728d25a90e59526284efada578d
Anton <jjoska@yandex.ru>
2020-12-14 03:15:32 +0300

    fix(hooks/useParams): now always returns an object

--- src/hooks/useParams.ts
+++ src/hooks/useParams.ts
@@ -5,5 +5,5 @@ import { useNavigator } from '../hooks'
  * */
 export function useParams() {
   let navigator = useNavigator()
-  return navigator.history.state
+  return navigator.history.state || {}
 }
