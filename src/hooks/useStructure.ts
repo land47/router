550cba98767c813f4fb03d09bcf05345b19ffce0
Anton <jjoska@yandex.ru>
2020-12-14 18:17:05 +0300

    feat(hooks/useStructure): hook now accepts options (#1)

--- src/hooks/useStructure.ts
+++ src/hooks/useStructure.ts
@@ -1,12 +1,15 @@
 import { useEffect } from 'react'
-import { ApplicationStructure } from '../shared/types'
+import { ApplicationStructure, HistoryItemState } from '../shared/types'
 import { useNavigator, useRouter, useSearch } from '../hooks'
 
 /**
  * Устанавливает структуру приложения и обновляет значения
  * в случае перехода на другое состояние навигации.
  * */
-export function useStructure<S extends ApplicationStructure>(initial: S) {
+export function useStructure<S extends ApplicationStructure, T>(
+  initial: S,
+  options: HistoryItemState<T> = {}
+) {
   let search = useSearch(Object.keys(initial))
   let navigator = useNavigator()
   let router = useRouter()
@@ -17,7 +20,7 @@ export function useStructure<S extends ApplicationStructure>(initial: S) {
 
     let hash = window.location.hash.slice(1)
 
-    router.replace(initial)
+    router.replace(initial, options)
 
     if (hash) {
       router.push(navigator.serialize(hash))
