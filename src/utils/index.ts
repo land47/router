9564b7ac5d320d8cf472cfcd3fd345d4f96eb9d1
Anton <jjoska@yandex.ru>
2020-12-16 00:03:22 +0300

    chore(utils): re-export `isEqualObjects()`

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export { isEqualArrays } from './isEqualArrays'
 export { hasIntersections } from './hasIntersections'
 export { withoutValue } from './withoutValue'
+export { isEqualObjects } from './isEqualObjects'
