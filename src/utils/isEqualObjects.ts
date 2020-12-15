f0355546b8e32fb373f83549e5004fcca87ad95f
Anton <jjoska@yandex.ru>
2020-12-16 00:02:56 +0300

    feat(utils/isEqualObjects): initialize helper

---
+++ src/utils/isEqualObjects.ts
@@ -0,0 +1,14 @@
+export function isEqualObjects<V, T extends Record<string, V>>(a: T, b: T) {
+  if (a === b) {
+    return true
+  }
+
+  let entries1 = Object.entries(a)
+  let entries2 = Object.entries(b)
+
+  if (entries1.length !== entries2.length) {
+    return false
+  }
+
+  return entries1.every(([key]) => a[key] === b[key])
+}
