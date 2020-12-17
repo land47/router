a64c7a8777585638d890e0e33da5d36ff97c315e
Anton <jjoska@yandex.ru>
2020-12-17 18:07:05 +0300

    refactor(hooks/useStructure): using the new method name

--- src/hooks/useStructure.ts
+++ src/hooks/useStructure.ts
@@ -18,9 +18,9 @@ export function useStructure<S extends ApplicationStructure, T>(
     let hash = window.location.hash.slice(1)
 
     // Фикс повторного первого рендера приложения
-    navigator.freeze()
+    navigator.freezeLifecycle()
     router.replace(initial, options)
-    navigator.unfreeze()
+    navigator.unfreezeLifecycle()
 
     if (hash) {
       router.push(navigator.convertSearchParams(hash))
