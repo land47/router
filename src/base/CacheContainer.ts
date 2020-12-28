d28ecdd725c2fd15c492530b66d0aadf3b145a03
Anton <jjoska@yandex.ru>
2020-12-28 09:51:55 +0300

    docs(base/CacheContainer): added comments

--- src/base/CacheContainer.ts
+++ src/base/CacheContainer.ts
@@ -5,6 +5,10 @@ export class CacheContainer<Key, Value> {
     this.container = new Map(initialValue)
   }
 
+  /**
+   * Если в контейнере есть ключ, возвращает значение по этому ключу.
+   * В обратном случае устанавливает значение по ключу, возвращая значение.
+   */
   load = (key: Key, value: Value) => {
     if (this.container.has(key)) {
       return this.container.get(key)
