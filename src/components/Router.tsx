66e6d9847780b1bf1426f63cd28467f2ada8239c
Anton <jjoska@yandex.ru>
2020-12-29 13:01:14 +0300

    feat(components/Router): wrapped in `PopoutProvider`

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -4,17 +4,20 @@ import {
   NavigatorProvider,
   SnackbarProvider,
   CacheContainerProvider,
+  PopoutProvider,
 } from './providers'
 
 /**
  * Оборачивает приложение в необходимые провайдеры.
- * */
+ */
 export let Router: FC = ({ children }) => {
   return (
     <CacheContainerProvider>
       <LaunchParamsProvider>
         <NavigatorProvider>
-          <SnackbarProvider>{children}</SnackbarProvider>
+          <SnackbarProvider>
+            <PopoutProvider>{children}</PopoutProvider>
+          </SnackbarProvider>
         </NavigatorProvider>
       </LaunchParamsProvider>
     </CacheContainerProvider>
