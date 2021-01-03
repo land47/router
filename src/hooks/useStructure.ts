9289cdcf0cf4de6f2e3c512ed7f4982dca593c85
Anton <jjoska@yandex.ru>
2021-01-03 09:36:22 +0300

    refactor(hooks/useStructure): code refactoring

--- src/hooks/useStructure.ts
+++ src/hooks/useStructure.ts
@@ -1,7 +1,12 @@
-import { useEffect, useMemo } from 'react'
+import { ReactNode, useEffect, useMemo } from 'react'
 import { ApplicationStructure, HistoryItemState } from '../shared/types'
 import { useNavigator, usePopout, useRouter, useSearch } from '../hooks'
 import { areObjectsEqual } from '../utils'
+
+type Structure<S extends ApplicationStructure> = S & {
+  modal: ReactNode
+  popout: ReactNode
+}
 
 /**
  * Устанавливает структуру приложения и обновляет значения
@@ -10,40 +15,37 @@ import { areObjectsEqual } from '../utils'
 export function useStructure<S extends ApplicationStructure, T>(
   initial: S,
   options: HistoryItemState<T> = {}
-) {
+): Structure<S> {
   let search = useSearch(Object.keys(initial)) as S
   let navigator = useNavigator()
   let router = useRouter()
-  let { popout } = usePopout()
 
   useEffect(() => {
     let hash = window.location.hash.slice(1)
 
     // Фикс повторного первого рендера приложения
     navigator.freezeLifecycle()
-    router.replace(initial, options).then(() => {
-      navigator.unfreezeLifecycle()
-
-      if (hash) {
-        if (areObjectsEqual(navigator.convertSearchParams(hash), initial)) {
+    router
+      .replace(initial, options)
+      .then(navigator.unfreezeLifecycle)
+      .then(() => {
+        // Если нет хеша, или начальная структура равна структуре,
+        // переданной хешем, пропускаем добавление записи.
+        if (
+          !hash ||
+          areObjectsEqual(navigator.convertSearchParams(hash), initial)
+        ) {
           return
         }
 
-        let original = navigator.convertSearchParams(hash)
-        let params = { ...original }
+        let serialized = navigator.convertSearchParams(hash)
 
-        // Удаляем из параметров данные о структуре приложения
-        delete params.panel
-        delete params.view
-        delete params.story
-
-        router.push(original, params)
-      }
-    })
+        // Не включаем данные о структуре в параметры.
+        // #panel=home&a=1&b=2 => { a: '1', b: '2' }
+        let params = (({ modal, story, view, panel, ...o }) => o)(serialized)
+        router.push(serialized, params)
+      })
   }, [])
 
-  return useMemo(() => ({ modal: null, ...(search || initial), popout }), [
-    search,
-    initial,
-  ])
+  return { modal: null, popout: usePopout().popout, ...(search || initial) }
 }
