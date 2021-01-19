44246a0cb0f7a3df7fcdc99a48c26a8b96dcc351
Anton <jjoska@yandex.ru>
2021-01-19 17:20:14 +0300

    fix(hooks/useParams): now grabs the parameters before unmounting

--- hooks/useParams.ts
+++ hooks/useParams.ts
@@ -1,8 +1,17 @@
+import { useEffect, useRef } from 'react'
 import { useSafeContext } from '.'
 import * as Contexts from '../contexts'
 
 /**
  * Возвращает состояние (параметры) текущей записи в истории.
  */
-export let useParams = () =>
-  useSafeContext(Contexts.Navigator).history.state || {}
+export let useParams = () => {
+  let navigator = useSafeContext(Contexts.Navigator)
+  let captured = useRef({})
+
+  useEffect(() => {
+    captured.current = navigator.history.state
+  }, [])
+
+  return { ...navigator.history.state, ...captured.current }
+}
