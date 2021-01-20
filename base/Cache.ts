bc70697873dbe91b036d3becd06915b223174549
Anton <jjoska@yandex.ru>
2021-01-20 16:07:24 +0300

    feat(base): caching

---
+++ base/Cache.ts
@@ -0,0 +1,37 @@
+export class Cache<Key, Value> {
+  /**
+   * Контейнер, в котором хранится кэшированные данные.
+   */
+  private readonly container: Map<Key, Value>
+
+  /**
+   * Максимальное количество элементов, одновременно хранящихся в контейнере.
+   */
+  static MAX_SIZE: number = 8
+
+  constructor(...initialValue: [Key, Value][]) {
+    this.container = new Map(initialValue)
+  }
+
+  /**
+   * Метод позволяет проверить есть ли в кэше
+   * элемент с переданным ключом.
+   */
+  has = (key: Key) => this.container.has(key)
+
+  /**
+   * Добавляет (или заменяет) значение по ключу в контейнер.
+   */
+  set = (key: Key, value: Value) => {
+    if (this.container.size === Cache.MAX_SIZE) {
+      this.container.delete(this.container.keys().next().value)
+    }
+
+    this.container.set(key, value)
+  }
+
+  /**
+   * Возвращает значение (или undefined) из контейнера по ключу.
+   */
+  get = (key: Key) => this.container.get(key)
+}
