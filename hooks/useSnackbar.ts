c73f16a64dd3901db378f6e8b5d6824ebac82bd7
Anton <jjoska@yandex.ru>
2021-01-20 16:10:52 +0300

    feat(hooks/useSnackbar): close the snackbar when switching to another navigation state

--- hooks/useSnackbar.ts
+++ hooks/useSnackbar.ts
@@ -1,9 +1,13 @@
-import { ReactNode } from 'react'
-import { useSafeContext } from '.'
+import { ReactNode, useEffect } from 'react'
+import { useNavigator, useSafeContext } from '.'
 import * as Contexts from '../contexts'
 
 export function useSnackbar() {
   let snackbar = useSafeContext(Contexts.Snackbar)
+  let navigator = useNavigator()
+
+  // Закрывает снэкбар при переходе на другое состояние навигации
+  useEffect(() => navigator.createPhantomTask(snackbar.close), [])
 
   return {
     setSnackbar: (node: ReactNode) => snackbar.set(node),
